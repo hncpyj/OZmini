3dabd8a54aa3288cd6ddf8934176364beebac72a
user2 <user2@DESKTOP-G2Q4DME>
2020-04-13 20:18:57 +0900

    mfqwefwqe
    wqer

---
+++ src/com/kh/model/vo/M_Bottle.java
@@ -0,0 +1,6 @@
+package com.kh.model.vo;
+
+public class M_Bottle extends M_Garbage{
+	
+
+}
--- src/com/kh/model/vo/M_Garbage.java
+++ src/com/kh/model/vo/M_Garbage.java
@@ -1,24 +1,14 @@
 package com.kh.model.vo;
 
 public class M_Garbage {
-	private int hp;
 	private int haveGarbage;
 	private int life = 10;
 	
-	
-	public M_Garbage(int hp, int haveGarbage) {
+	public M_Garbage(int haveGarbage) {
 		super();
-		this.hp = hp;
 		this.haveGarbage = haveGarbage;
 	}
 	
-	
-	public int getHp() {
-		return hp;
-	}
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
 	public int getHaveGargabe() {
 		return haveGarbage;
 	}
