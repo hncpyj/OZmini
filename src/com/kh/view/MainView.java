126310fdcc9af0f2f549f551d624f7f5039579df
hailey <hailey@192.168.100.27>
2020-04-14 01:36:23 +0900

    minigameview
    plyz.....

--- src/com/kh/view/MainView.java
+++ src/com/kh/view/MainView.java
@@ -7,11 +7,17 @@ import com.kh.model.vo.Player;
 
 
 public class MainView extends JFrame {
-	Player p = new Player();	
+	
 	public MainView() {
+		Player p = new Player();
 		this.setTitle("oz의 펭귄");
 		this.setBounds(0, 0, 380, 680);
 		
+		
+		MiniGameView mgv = new MiniGameView(this, p);
+		Thread th = new Thread(mgv);
+		th.start();
+		this.addKeyListener(mgv);
 		this.add(new MiniGameView(this, p));
 		
 		this.setVisible(true);
