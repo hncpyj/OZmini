ce7c00cad63e51eb3e97ef0f78d366051d612c4c
user2 <user2@DESKTOP-G2Q4DME>
2020-04-13 21:27:47 +0900

    M_Bottle
    ghn

--- src/com/kh/model/vo/M_Garbage.java
+++ src/com/kh/model/vo/M_Garbage.java
@@ -1,25 +1,5 @@
 package com.kh.model.vo;
 
 public class M_Garbage {
-	private int haveGarbage;
-	private int life = 10;
-	
-	public M_Garbage(int haveGarbage) {
-		super();
-		this.haveGarbage = haveGarbage;
-	}
-	
-	public int getHaveGargabe() {
-		return haveGarbage;
-	}
-	public void setHaveGargabe(int haveGarbage) {
-		this.haveGarbage = haveGarbage;
-	}
-	
-	public int getLife() {
-		return life;
-	}
-	public void setLife(int life) {
-		this.life = life;
-	}
+	private int count;
 }
